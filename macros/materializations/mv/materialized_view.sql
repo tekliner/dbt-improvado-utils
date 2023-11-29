e8d7b88babfebc1fcaa7a0b27fc8e7b4319a83f0
Oleg Guralnik <oleg.guralnik@improvado.io>
2023-11-29 23:50:03 +0600

    import mat view and fix create custom prefix

---
+++ macros/materializations/mv/materialized_view.sql
@@ -0,0 +1,89 @@
+{% macro create_materialized_view_as(relation, target, sql) -%}
+    CREATE MATERIALIZED VIEW {{ relation }} TO {{ target }} AS (
+        {{ sql }}
+    )
+{% endmacro %}
+
+
+
+{% materialization materialized_view, adapter='clickhouse' -%}
+    {% set target_table_exists, target_table = get_or_create_relation(database=this.database, schema=this.schema, identifier=this.identifier, type='table') -%}
+    {% set existing_target_table = load_cached_relation(target_table) %}
+
+    {% set prefix ='_mv_' %}
+    {% set mv_identifier = prefix ~ this.identifier %}
+    {% set target_matview = this.incorporate(path={"identifier": mv_identifier}) %}
+    {% set existing_matview = load_cached_relation(target_matview) %}
+
+    {% set tmp_relation = make_intermediate_relation(target_table) %}
+    {% do drop_relation_if_exists(tmp_relation) %}
+
+    {{ run_hooks(pre_hooks, inside_transaction=False) }}
+    -- `BEGIN` happens here:
+    {{ run_hooks(pre_hooks, inside_transaction=True) }}
+
+    {% set to_drop = [] %}
+    {% do create_temporary_empty_table(tmp_relation, sql) %}
+    {{ to_drop.append(tmp_relation) }}
+
+    {% if existing_matview is not none %}
+        -- check schema consistency if matview exists
+        {% set schema_changes_dict = check_for_schema_changes(tmp_relation, existing_matview) %}
+        {% if schema_changes_dict['schema_changed'] or existing_target_table is none %}
+            {% set full_rebuild = True %}
+        {% endif %}
+        {% do log("MV exists, schema_changed=" ~ schema_changes_dict['schema_changed'] ~ ", existing_target_table=" ~ existing_target_table, True) %}
+
+    {% else %}
+        -- matview doesnt exist, full build checks
+        {% if existing_target_table is not none %}
+            -- target exists, check schema consistency
+            {% set schema_changes_dict = check_for_schema_changes(tmp_relation, existing_target_table) %}
+            {% if schema_changes_dict['schema_changed'] %}
+                -- target inconsistent, full rebuild
+                {% set full_rebuild = True %}
+            {% endif %}
+            -- target consistent, create matview only
+            {% set create_matview = True %}
+            {% do log("target exists, schema_changed=" ~ schema_changes_dict['schema_changed'], True) %}
+
+        {% else %}
+            -- target doesnt exist, full rebuild
+            {% set full_rebuild = True %}
+            {% do log("target doesn't exist, full build", True) %}
+        {% endif %}
+
+    {% endif %}
+
+    {% if full_rebuild %}
+        {% do materialize_table(target_table, sql) %}
+    {% endif %}
+
+    {% if full_rebuild or create_matview %}
+        {% do materialize_matview(target_matview, target_table, sql) %}
+
+        {{ run_hooks(post_hooks, inside_transaction=True) }}
+
+          -- cleanup
+        {% set should_revoke = should_revoke(existing_target_table, full_refresh_mode=True) %}
+        {% do apply_grants(target_table, grant_config, should_revoke=should_revoke) %}
+
+        {% do persist_docs(target_table, model) %}
+
+        -- `COMMIT` happens here
+        {% do adapter.commit() %}
+
+    {% else %}
+        -- for dry run 
+        {{ store_result('main', 'SKIP') }}
+    {% endif %}
+
+    {% for rel in to_drop %}
+        {% do adapter.drop_relation(rel) %}
+    {% endfor %}
+
+    {{ run_hooks(post_hooks, inside_transaction=False) }}
+
+    {{ return({'relations': [target_table, target_matview]}) }}
+
+{%- endmaterialization %}
---
+++ macros/materializations/mv/utils.sql
@@ -0,0 +1,113 @@
+{% macro create_temporary_empty_table(relation, sql) %}
+    {% set wrapped_sql %}
+        WITH
+            __schema_template as (
+                {{ sql }}
+            )
+        SELECT *
+        FROM __schema_template
+        WHERE 1=0
+    {% endset %}
+
+    {% call statement('tmp_relation_table') -%}
+        {{ clickhouse__create_table_as(False, relation, wrapped_sql, True)  }}
+    {%- endcall %}
+
+    {{ return(relation) }}
+{% endmacro %}
+
+
+{% macro materialize_table(_this, sql) %}
+    {%- set existing_relation = load_cached_relation(_this) -%}
+    {%- set target_relation = _this.incorporate(type='table') -%}
+    {%- set backup_relation = none -%}
+    {%- set preexisting_backup_relation = none -%}
+    {%- set preexisting_intermediate_relation = none -%}
+
+    {% if existing_relation is not none %}
+        {%- set backup_relation_type = existing_relation.type -%}
+        {%- set backup_relation = make_backup_relation(target_relation, backup_relation_type) -%}
+        {%- set preexisting_backup_relation = load_cached_relation(backup_relation) -%}
+        {% if not existing_relation.can_exchange %}
+            {%- set intermediate_relation = make_intermediate_relation(target_relation) -%}
+            {%- set preexisting_intermediate_relation = load_cached_relation(intermediate_relation) -%}
+        {% endif %}
+    {% endif %}
+
+    -- drop the temp relations if they exist already in the database
+    {{ drop_relation_if_exists(preexisting_intermediate_relation) }}
+    {{ drop_relation_if_exists(preexisting_backup_relation) }}
+
+    {% if backup_relation is none %}
+        {{ log('Creating new relation ' + target_relation.name )}}
+        -- There is not existing relation, so we can just create
+        {% call statement('main') -%}
+            {{ create_table_as(False, target_relation, sql) }}
+        {%- endcall %}
+    {% elif existing_relation.can_exchange %}
+        -- We can do an atomic exchange, so no need for an intermediate
+        {% call statement('main') -%}
+            {{ create_table_as(False, backup_relation, sql) }}
+        {%- endcall %}
+        {% do exchange_tables_atomic(backup_relation, existing_relation) %}
+    {% else %}
+        -- We have to use an intermediate and rename accordingly
+        {% call statement('main') -%}
+            {{ create_table_as(False, intermediate_relation, sql) }}
+        {%- endcall %}
+        {{ adapter.rename_relation(existing_relation, backup_relation) }}
+        {{ adapter.rename_relation(intermediate_relation, target_relation) }}
+    {% endif %}
+
+    {{ drop_relation_if_exists(backup_relation) }}
+
+    {{ return({'relations': [target_relation]}) }}
+{% endmacro %}
+
+
+{% macro materialize_matview(_this, mv_target_relation, sql) %}
+    {%- set existing_relation = load_cached_relation(_this) -%}
+    {%- set target_relation = _this.incorporate(type='table') -%}
+    {%- set backup_relation = none -%}
+    {%- set preexisting_backup_relation = none -%}
+    {%- set preexisting_intermediate_relation = none -%}
+
+    {% if existing_relation is not none %}
+        {%- set backup_relation_type = existing_relation.type -%}
+        {%- set backup_relation = make_backup_relation(target_relation, backup_relation_type) -%}
+        {%- set preexisting_backup_relation = load_cached_relation(backup_relation) -%}
+        {% if not existing_relation.can_exchange %}
+            {%- set intermediate_relation = make_intermediate_relation(target_relation) -%}
+            {%- set preexisting_intermediate_relation = load_cached_relation(intermediate_relation) -%}
+        {% endif %}
+    {% endif %}
+
+    -- drop the temp relations if they exist already in the database
+    {{ drop_relation_if_exists(preexisting_intermediate_relation) }}
+    {{ drop_relation_if_exists(preexisting_backup_relation) }}
+
+    {% if backup_relation is none %}
+        {{ log('Creating new relation ' + target_relation.name )}}
+        -- There is not existing relation, so we can just create
+        {% call statement('main') -%}
+            {{ create_materialized_view_as(target_relation, mv_target_relation, sql) }}
+        {%- endcall %}
+    {% elif existing_relation.can_exchange %}
+        -- We can do an atomic exchange, so no need for an intermediate
+        {% call statement('main') -%}
+            {{ create_materialized_view_as(backup_relation, mv_target_relation, sql) }}
+        {%- endcall %}
+        {% do exchange_tables_atomic(backup_relation, existing_relation) %}
+    {% else %}
+        -- We have to use an intermediate and rename accordingly
+        {% call statement('main') -%}
+            {{ create_materialized_view_as(intermediate_relation, mv_target_relation, sql) }}
+        {%- endcall %}
+        {{ adapter.rename_relation(existing_relation, backup_relation) }}
+        {{ adapter.rename_relation(intermediate_relation, target_relation) }}
+    {% endif %}
+
+    {{ drop_relation_if_exists(backup_relation) }}
+
+    {{ return({'relations': [target_relation]}) }}
+{% endmacro %} 
