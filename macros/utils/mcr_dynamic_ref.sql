1b9971487010683dd4f188c51f2a8e282abcf1bd
Maxim Solovev <maxim.solovev@improvado.io>
2024-05-16 13:33:01 +0500

    BI-6519-implement-dynamic-limit-for-materialization-on-pr-check(update 2) (#24)
    * Create mcr_set_dev_limit.sql
    
    * Update and rename mcr_set_dev_limit.sql to mcr_dynamic_ref.sql
    
    * Update mcr_dynamic_ref.sql
    
    * Update mcr_dynamic_ref.sql
    
    * Update mcr_dynamic_ref.sql

--- macros/utils/mcr_dynamic_ref.sql
+++ macros/utils/mcr_dynamic_ref.sql
@@ -1,5 +1,5 @@
 {%- macro mcr_dynamic_ref(relation, rows_limit=1000) -%}
-    {%- if target.schema != 'internal_analytics' and model.config.materialized not in ('view','seed') -%}
+    {%- if target.schema not in ('default', 'internal_analytics', 'internal_analytics_src') and model.config.materialized not in ('view','seed') -%}
         {{ return(ref(relation) ~ ' limit ' ~ rows_limit) }}
     {%- else -%}
         {{ ref(relation) }}
