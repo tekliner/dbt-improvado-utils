8e835779f75f1e9c10f96acaa7a1d7d336f66b72
Maxim Solovev <maxim.solovev@improvado.io>
2024-06-03 17:45:41 +0500

    Update mcr_drop_relation_if_exists.sql

--- macros/utils/mcr_drop_relation_if_exists.sql
+++ macros/utils/mcr_drop_relation_if_exists.sql
@@ -1,6 +1,14 @@
 {%- macro mcr_drop_relation_if_exists(relation) -%}
     {%- set relation_name = relation.identifier -%}
-    {%- set relation_type = run_query('select engine from system.tables where name = ' ~ "'" ~ relation_name ~ "'") -%}
+    {%- set relation_type_query %}
+        select
+            if(engine = 'Dictionary', engine, 'Table')
+        from
+            system.tables
+        where
+            name = '{{ relation_name }}'
+    {%- endset -%}
+    {% set relation_type = run_query(relation_type_query) %}
 
     {% if relation_type %}
         {%- do run_query('drop ' ~ relation_type[0][0] ~ ' if exists ' ~ relation_name) -%}
