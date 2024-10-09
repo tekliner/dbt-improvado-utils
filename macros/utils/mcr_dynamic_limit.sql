ca4b3c8e66ab4cbb4afa108c46db5384897f5d9a
Maxim Solovev <maxim.solovev@improvado.io>
2024-10-09 17:25:11 +0500

    Delete macros/utils/mcr_dynamic_limit.sql

--- macros/utils/mcr_dynamic_limit.sql
+++
@@ -1,5 +0,0 @@
-{%- macro mcr_dynamic_limit(rows_limit=1000) -%}
-    {%- if target.schema not in ('default', 'internal_analytics', 'internal_analytics_src') -%}
-        {{ return('limit ' ~ rows_limit) }}
-    {%- endif -%}
-{%- endmacro -%}
