afc1b393ad3daec35735ce67567a6de81c8c1dbd
Oleg Guralnik <oleg.guralnik@improvado.io>
2025-04-07 15:20:19 +0400

    adjust for improvado models

--- macros/utils/mcr_source_or_ref.sql
+++ macros/utils/mcr_source_or_ref.sql
@@ -1,7 +1,7 @@
 {%- macro mcr_source_or_ref(src_yml, table_name) -%}
     {%- set is_parent_on_staging_exists = adapter.get_relation(database=none, schema=target.schema, identifier=table_name) -%}
 
-    {%- if target.schema.startswith('internal_analytics_dbt_staging') and is_parent_on_staging_exists -%}
+    {%- if 'staging' in target.schema and is_parent_on_staging_exists -%}
         {{ ref(table_name) }}
     {%- else -%}
         {{ source(src_yml, table_name) }}
