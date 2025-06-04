363ffff118c7092e5d7f2c94535b6bf6af999bf1
Maxim Solovev <maxim.solovev@improvado.io>
2025-06-04 13:49:44 +0500

    Bi 7802 implement new materialization for dictionaries on dev staging (#41)
    * BI-8140-update-mcr-source-or-ref-for-improvado-models (#38)
    adjust for improvado models
    
    * Update mcr_is_prod_schema.sql
    
    * Update dictionary.sql
    
    * update
    
    * upd
    
    * Update dictionary.sql
    
    * Update mcr_dynamic_limit.sql

--- macros/utils/mcr_dynamic_limit.sql
+++ macros/utils/mcr_dynamic_limit.sql
@@ -1,6 +1,6 @@
 {%- macro mcr_dynamic_limit(rows_limit=1000) -%}
 
-    {%- if not mcr_is_prod_schema() and not var('ignore_dev_limits') -%}
+    {%- if not dbt_improvado_utils.mcr_is_prod_schema() and not var('ignore_dev_limits') -%}
         {{ return('limit ' ~ rows_limit) }}
     {%- endif -%}
 
