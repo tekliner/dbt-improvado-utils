c52aa2344a10840241440a5a69b13aced2b531f7
Nikita Manakov <nikita.manakov@improvado.io>
2025-08-08 17:54:32 +0500

    feat(utils): Refactor macros to improve schema checks in mcr_dynamic_limit and mcr_source_or_ref (#50)
    * feat(utils): Refactor macros to improve schema checks in mcr_dynamic_limit and mcr_source_or_ref
    
    * Update mcr_source_or_ref.sql

--- macros/utils/mcr_source_or_ref.sql
+++ macros/utils/mcr_source_or_ref.sql
@@ -1,7 +1,7 @@
 {%- macro mcr_source_or_ref(src_yml, table_name) -%}
-    {%- set is_parent_on_staging_exists = adapter.get_relation(database=none, schema=target.schema, identifier=table_name) -%}
+    {%- set is_parent_node_exists = adapter.get_relation(database=none, schema=target.schema, identifier=table_name) -%}
 
-    {%- if 'staging' in target.schema and is_parent_on_staging_exists -%}
+    {%- if not dbt_improvado_utils.mcr_is_prod_schema() and is_parent_node_exists -%}
         {{ ref(table_name) }}
     {%- else -%}
         {{ source(src_yml, table_name) }}
