5264bc98ad2f93b7c507d9da4697c5fa16d111b0
Nikita Manakov <nikita.manakov@improvado.io>
2025-08-08 17:49:57 +0500

    Update mcr_source_or_ref.sql

--- macros/utils/mcr_source_or_ref.sql
+++ macros/utils/mcr_source_or_ref.sql
@@ -1,7 +1,7 @@
 {%- macro mcr_source_or_ref(src_yml, table_name) -%}
     {%- set is_parent_node_exists = adapter.get_relation(database=none, schema=target.schema, identifier=table_name) -%}
 
-    {%- if not dbt_improvado_utils.mcr_production_schema() and is_parent_node_exists -%}
+    {%- if not dbt_improvado_utils.mcr_is_prod_schema() and is_parent_node_exists -%}
         {{ ref(table_name) }}
     {%- else -%}
         {{ source(src_yml, table_name) }}
