1fd1f4d5773ac5da67e7f02b7d90496f6274b901
solovechik <msol294@gmail.com>
2025-12-18 15:21:32 +0500

    log fix

--- macros/materializations/microbatch.sql
+++ macros/materializations/microbatch.sql
@@ -682,8 +682,8 @@
     {%- for dup_group in duplicate_groups -%}
         {%- set dup_partition_id = dup_group['partition_id'] -%}
         {%- set dup_hash = dup_group['hash_of_all_files'] -%}
-        {%- set part_names = dup_group['part_names'] -%}
-        {%- set parts_to_delete = fromjson(part_names)[1:] -%}
+        {%- set part_names = fromjson(dup_group['part_names']) -%}
+        {%- set parts_to_delete = part_names[1:] -%}
 
         {{- diu.mcr_log_colored(
                 'Detected ' ~ (part_names | length) ~ ' duplicate parts in partition '~
